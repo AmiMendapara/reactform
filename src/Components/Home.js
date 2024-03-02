1a9dde993f17534d56f2a6b6986bb334c4728707
AmiMendapara <amipatel42429@gmail.com>
2024-03-02 09:53:54 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h2>Login Successfully</h2><hr/>
+      <h1>Welcome to Home Page</h1>
+    </>
+  );
+}
+
+export default Home;
